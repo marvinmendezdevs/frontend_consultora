760ffa2db1d3c6d89efadcb2db8cbc1b9023a1be
Alessandro Martinez <alessandrojosue53@gmail.com>
2026-01-07 21:11:50 -0600

    Empezando formulario para remediación y refuerzo

--- src/components/pages/Remediation.tsx
+++ src/components/pages/Remediation.tsx
@@ -16,55 +16,113 @@ function Remediation() {
     if (isError) return <p>Error: {isError}</p>;
     if (data) return (
     <div>
-        <h1 className="text-xl font-semibold my-2">Remedicación y refuerzo</h1>
-        <div className="w-full table-fixed overflow-x-auto">
-          <div className="grid grid-cols-10 bg-gray-100 p-1 w-full">
-            <div className="col-span-1 w-36">
-              <p>Grado</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Tipo</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Opción</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Sección</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Turno</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Materia</p>
-            </div>
-            <div className="col-span-1 w-38">
-              <p>Docente</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Dui</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Phone</p>
-            </div>
-            <div className="col-span-1 w-36">
-              <p>Email</p>
-            </div>
-          </div>
-          <div className="grid grid-cols-10 gap-2 w-full">
-            {data.map((item: SectionItem) => (
-              <div key={item.id}>
-                <p>{item.grade}</p>
-                <p>{item.track}</p>
-                <p>{item.subtrack}</p>
-                <p>{item.sectionClass}</p>
-                <p>{item.shift}</p>
-                <p>lenguaje</p>
-                <p>lenguaje</p>
-                <p>lenguaje</p>
-                <p>lenguaje</p>
-                <p>lenguaje</p>
+        <h1 className="text-xl font-semibold my-6">Remedicación y refuerzo</h1>
+        <div>
+          <div className="w-full overflow-x-auto">
+            <div className="min-w-[900px]">
+
+              <div className="grid grid-cols-10 bg-gray-100 p-1 w-full text-center">
+                <div className="py-2">Grado</div>
+                <div className="py-2">Tipo</div>
+                <div className="py-2">Opción</div>
+                <div className="py-2">Sección</div>
+                <div className="py-2">Turno</div>
+                <div className="py-2">Materia</div>
+                <div className="py-2">Docente</div>
+                <div className="py-2">Dui</div>
+                <div className="py-2">Phone</div>
+                <div className="py-2">Email</div>
               </div>
-            ))}
+
+              {data.map((item: SectionItem, index: number) => (
+                <div
+                  key={index + 1}
+                  className="w-full border border-gray-200 rounded-sm flex flex-col mb-3 max-h-96"
+                >
+                  <div className="w-full divide-y divide-gray-200">
+
+                    <div className="grid grid-cols-10 text-center">
+                      <p className="h-14 text-xs py-5">{item.grade}</p>
+                      <p className="h-14 text-xs py-5">{item.track}</p>
+                      <p className="h-14 text-xs py-5">{item.subtrack}</p>
+                      <p className="h-14 text-xs py-5">{item.sectionClass}</p>
+                      <p className="h-14 text-xs py-5">{item.shift}</p>
+                      <p className="h-14 text-xs py-5">lenguaje</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                    </div>
+
+                    <div className="grid grid-cols-10 text-center">
+                      <p className="h-14 text-xs py-5">{item.grade}</p>
+                      <p className="h-14 text-xs py-5">{item.track}</p>
+                      <p className="h-14 text-xs py-5">{item.subtrack}</p>
+                      <p className="h-14 text-xs py-5">{item.sectionClass}</p>
+                      <p className="h-14 text-xs py-5">{item.shift}</p>
+                      <p className="h-14 text-xs py-5">matematica</p>
+                      <p className="h-14 text-xs py-5 truncate"></p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                    </div>
+
+                    <div className="grid grid-cols-10 text-center">
+                      <p className="h-14 text-xs py-5">{item.grade}</p>
+                      <p className="h-14 text-xs py-5">{item.track}</p>
+                      <p className="h-14 text-xs py-5">{item.subtrack}</p>
+                      <p className="h-14 text-xs py-5">{item.sectionClass}</p>
+                      <p className="h-14 text-xs py-5">{item.shift}</p>
+                      <p className="h-14 text-xs py-5">remediación lenguaje</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                    </div>
+
+                    <div className="grid grid-cols-10 text-center">
+                      <p className="h-14 text-xs py-5">{item.grade}</p>
+                      <p className="h-14 text-xs py-5">{item.track}</p>
+                      <p className="h-14 text-xs py-5">{item.subtrack}</p>
+                      <p className="h-14 text-xs py-5">{item.sectionClass}</p>
+                      <p className="h-14 text-xs py-5">{item.shift}</p>
+                      <p className="h-14 text-xs py-5">remediación matematica</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                    </div>
+
+                    <div className="grid grid-cols-10 text-center">
+                      <p className="h-14 text-xs py-5">{item.grade}</p>
+                      <p className="h-14 text-xs py-5">{item.track}</p>
+                      <p className="h-14 text-xs py-5">{item.subtrack}</p>
+                      <p className="h-14 text-xs py-5">{item.sectionClass}</p>
+                      <p className="h-14 text-xs py-5">{item.shift}</p>
+                      <p className="h-14 text-xs py-5">refuerzo lenguaje</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                    </div>
+
+                    <div className="grid grid-cols-10 text-center">
+                      <p className="h-14 text-xs py-5">{item.grade}</p>
+                      <p className="h-14 text-xs py-5">{item.track}</p>
+                      <p className="h-14 text-xs py-5">{item.subtrack}</p>
+                      <p className="h-14 text-xs py-5">{item.sectionClass}</p>
+                      <p className="h-14 text-xs py-5">{item.shift}</p>
+                      <p className="h-14 text-xs py-5">refuerzo matematica</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                      <p className="h-14 text-xs py-5">-</p>
+                    </div>
+
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
     </div>
