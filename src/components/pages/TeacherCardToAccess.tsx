67af4019bf74a6a3e58f7a5e382d13d1d3f1e3c7
marvinmendezlearn <marvinmendezdevs@gmail.com>
2026-01-09 16:41:47 -0600

    comenzando con accesos por seccion

--- src/components/pages/TeacherCardToAccess.tsx
+++ src/components/pages/TeacherCardToAccess.tsx
@@ -22,29 +22,21 @@ function TeacherCardToAccess({ teacher }: TeacherCardToAccessProps) {
         }
     });
 
-    const handleActiveAccess = (teacherId: TeacherType['id']) => {
+/*     const handleActiveAccess = (teacherId: TeacherType['id']) => {
         mutation.mutate(teacherId);
-    }
+    } */
 
     return (
-        <div className="flex items-center gap-2 py-2" key={teacher.id}>
-            <div className="flex items-center justify-center size-10 rounded-full bg-indigo-600 text-white border-2 border-green-600">
-                {teacher.name[0]}
-            </div>
-            <div className="flex-1">
-                <p>{teacher.name}</p>
+        <div key={teacher.id}>
+            <div className="flex items-center gap-2 py-2">
+                <div className="flex items-center justify-center size-10 rounded-full bg-indigo-600 text-white border-2 border-green-600">
+                    {teacher.name[0]}
+                </div>
+                <div className="flex-1">
+                    <p>{teacher.name}</p>
+                </div>
             </div>
             <div>
-                {teacher.teacherAccess ? (
-                    <p className="italic text-xs text-green-600">Acceso verificado</p>
-                ) : (
-                    <button className="text-xs bg-gray-100 p-2 rounded-lg cursor-pointer hover:bg-gray-200 active:scale-95 disabled:opacity-50"
-                        onClick={ () => handleActiveAccess(teacher.id) }
-                        disabled={ mutation.isPending }
-                    >
-                        {mutation.isPending ? "Guardando" : "Marcar acceso"}
-                    </button>
-                )}
             </div>
         </div>
     )
