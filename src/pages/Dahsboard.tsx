9582ee1ba9ea42467cb989e3d73708cba17d6067
Marvin Méndez <133824886+marvinmendezdevs@users.noreply.github.com>
2026-01-31 19:19:19 -0600

    Agregando dashboard de secciones y demas.

--- src/pages/Dahsboard.tsx
+++ src/pages/Dahsboard.tsx
@@ -1,36 +1,51 @@
-import { LayoutDashboard } from "lucide-react";
+import { useNavbar } from "@/stores/index.store";
+import { LayoutDashboard, Menu, X } from "lucide-react";
 import { Link, Outlet, useLocation } from "react-router";
 
 function Dahsboard() {
+    const isOpenNavBar = useNavbar(state => state.isOpen)
+    const toggleNavBar = useNavbar(state => state.setIsOpen)
 
     const { pathname } = useLocation();
 
     return (
-        <div className="bg-linear-to-br from-indigo-50 via-white to-indigo-50 min-h-screen grid gap-2 lg:grid-cols-[200px_1fr]">
-            <aside className="flex flex-col gap-4 p-4">
-                <Link className={`${pathname === "/dashboard" ? "bg-linear-to-br from-white via-indigo-50 to-white" : ""} p-2 rounded-lg`} to={"/dashboard"}>
+        <div className="bg-linear-to-br from-indigo-50 via-white to-indigo-50 h-screen overflow-hidden flex flex-col lg:grid lg:grid-cols-[200px_1fr]">
+            <aside className={`fixed inset-0 flex flex-col gap-4 p-4 bg-linear-to-br from-indigo-50 via-white to-indigo-50 transition lg:bg-none lg:static lg:translate-0 ${isOpenNavBar ? "translate-x-0" : "-translate-x-full"}`}>
+                <div className="flex justify-end lg:hidden">
+                    <button onClick={ toggleNavBar }>
+                        <X />
+                    </button>
+                </div>
+                <Link className={`${pathname === "/dashboard" ? "bg-linear-to-br from-white via-indigo-50 to-white text-indigo-600" : ""} p-2 rounded-lg font-semibold`} to={"/dashboard"}>
                     Docentes
                 </Link>
-                <Link className={`${pathname === "/dashboard/secciones" ? "bg-linear-to-br from-white via-indigo-50 to-white" : ""} p-2 rounded-lg`} to={"/dashboard/secciones"}>
+                <Link className={`${pathname === "/dashboard/secciones" ? "bg-linear-to-br from-white via-indigo-50 to-white text-indigo-600" : ""} p-2 rounded-lg font-semibold`} to={"/dashboard/secciones"}>
                     Secciones
                 </Link>
-                <Link className={`${pathname === "/dashboard/estudiantes" ? "bg-linear-to-br from-white via-indigo-50 to-white" : ""} p-2 rounded-lg`} to={"/dashboard/estudiantes"}>
+                <Link className={`${pathname === "/dashboard/estudiantes" ? "bg-linear-to-br from-white via-indigo-50 to-white text-indigo-600" : ""} p-2 rounded-lg font-semibold`} to={"/dashboard/estudiantes"}>
                     Estudiantes
                 </Link>
             </aside>
 
-            <div className="w-11/12 max-w-7xl mx-auto py-3">
-                <header className="flex flex-col md:flex-row md:items-center justify-between gap-4">
-                    <div>
-                        <div className="flex items-center gap-2 text-indigo-600 mb-1">
-                            <LayoutDashboard size={20} />
-                            <span className="text-sm font-semibold uppercase tracking-wider">Sistema de Gestión</span>
-                        </div>
-                        <h1 className="text-3xl font-bold text-slate-900 tracking-tight">Informe de progresos</h1>
-                    </div>
-                </header>
-                <Outlet />
+            <div className="flex justify-end p-3 mb-3 lg:hidden">
+                <button  onClick={ toggleNavBar }>
+                    <Menu />
+                </button>
             </div>
+            <main className="flex-1 overflow-auto">
+                <div className="w-11/12 max-w-7xl mx-auto py-3">
+                    <header className="flex flex-col md:flex-row md:items-center justify-between gap-4">
+                        <div>
+                            <div className="flex items-center gap-2 text-indigo-600 mb-1">
+                                <LayoutDashboard size={20} />
+                                <span className="text-sm font-semibold uppercase tracking-wider">Sistema de Gestión</span>
+                            </div>
+                            <h1 className="text-3xl font-bold text-slate-900 tracking-tight">Informe de progresos</h1>
+                        </div>
+                    </header>
+                    <Outlet />
+                </div>
+            </main>
         </div>
     )
 }
